d2ce9ebae6c00d553a88d580993fbdbe938644c7
MarkoTeov <108360992+MarkoTeov@users.noreply.github.com>
2022-07-31 22:38:02 +0200

    Homework 9
    Homework for JS 1

---
+++ homework9.js
@@ -0,0 +1,27 @@
+let myName = "Marko";
+let age = 29; 
+let jobTitle = "QA Engineer";
+let gender = "male";
+let isFromSkopje = true; 
+
+let person = {
+    personName: "Marko",
+    personAge: 29,
+    personJobTitle: "QA Engineer",
+    isPersonFromSkopje: true
+}
+
+function showMyName() {
+    alert("My name is" + " " + myName);
+  } 
+  // Ова е функција за нотификација во вид на alert за приказ на името 
+
+  function showMyAge() {
+    alert("My age is" + " " + age)
+  }
+  // Ова е функција за нотификација во вид на alert за приказ на годините
+
+function showMyJobTitle() {
+    alert("My job title is" + " " + jobTitle)
+}
+// Ова е функција за нотификација во вид на alert за приказ на работна позиција
